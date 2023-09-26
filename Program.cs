837d8b425705705cbb4ef86770e0d178e74e592d
YokairaTaveras <143297254+YokairaTaveras@users.noreply.github.com>
2023-09-25 23:18:56 -0400

    Inyecte el context en el program

--- Program.cs
+++ Program.cs
@@ -1,14 +1,16 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
-using Yokaira_AP1_P1.Data;
+using Microsoft.EntityFrameworkCore;
+using Yokaira_AP1_P1.DAL;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
+var ConStr = builder.Configuration.GetConnectionString("ConStr");
 
+builder.Services.AddDbContext<Context>(op => op.UseSqlite(ConStr));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
