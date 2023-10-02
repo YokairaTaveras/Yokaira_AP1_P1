5e7d49c69a9bec6e1515324710b0d9cc63436a98
YokairaTaveras <143297254+YokairaTaveras@users.noreply.github.com>
2023-10-02 18:21:04 -0400

    Creando el modelo

---
+++ Models/Aportes.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Yokaira_AP1_P1.Models
+{
+    public class Aportes
+    {
+        [Key]
+        public int AportesId { get; set; }
+
+        [Required(ErrorMessage = "Este campo es necesario")]
+        public DateTime Fecha { get; set; }
+
+        [Required(ErrorMessage = "Este campo es necesario")]
+        public string Observacion { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Este campo es necesario")]
+        [Range(1, 5000)]
+        public int Monto { get; set; }
+    }
+}
+
